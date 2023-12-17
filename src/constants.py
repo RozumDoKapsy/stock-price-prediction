8971f6b63553908673d5df85ca3f7b3f29ce9fc8
RozumDoKapsy <sulda.str@gmail.com>
2023-12-17 14:38:52 +0100

    feat: update path to ROOT

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
 import os
 from pathlib import Path
 
-ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__))).parent
+ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__))).parent.resolve()
 PATH_TO_RAW_DATA = os.path.join(ROOT_DIR, 'data/raw')
