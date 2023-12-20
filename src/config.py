09dea223bfa75baef04832bedb5afdaa02e98679
RozumDoKapsy <sulda.str@gmail.com>
2023-12-20 09:51:55 +0100

    feat: new PATHS to features and models

--- src/config.py
+++ src/config.py
@@ -3,3 +3,5 @@ from pathlib import Path
 
 ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__))).parent
 PATH_TO_DATA = os.path.join(ROOT_DIR, 'bucket/data')
+PATH_TO_FEATURES = os.path.join(ROOT_DIR, 'bucket/features')
+PATH_TO_MODELS = os.path.join(ROOT_DIR, 'bucket/models')
