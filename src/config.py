9339f53d7ce3eba96758fd85cddfe5f3e13dedb8
RozumDoKapsy <sulda.str@gmail.com>
2023-12-22 10:15:05 +0100

    feat: set parameters for feature extraction - LAG, TEST_SIZE_PCT

--- src/config.py
+++ src/config.py
@@ -5,3 +5,6 @@ ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__))).parent
 PATH_TO_DATA = os.path.join(ROOT_DIR, 'bucket/data')
 PATH_TO_FEATURES = os.path.join(ROOT_DIR, 'bucket/features')
 PATH_TO_MODELS = os.path.join(ROOT_DIR, 'bucket/models')
+
+TEST_SIZE_PCT = 0.05
+LAG = 60
